3bc0dac34a0ad00045ce9ee8516e5bf5978e0204
simonxu72 <simon.xu72@gmail.com>
2017-11-22 22:45:20 +0800

    add convert_config @ resp_query

--- src/pg_mcht_protocol_resp_query.erl
+++ src/pg_mcht_protocol_resp_query.erl
@@ -25,6 +25,7 @@
 -export([
   sign_fields/0
   , options/0
+  , convert_config/0
 ]).
 %% callbacks of pg_protocol
 %%-------------------------------------------------------------------
@@ -77,5 +78,31 @@ options() ->
 
 convert_config() ->
   [
+    {normal_resp,
+      [
+        {to, ?MODULE},
+        {from,
+          [
+            {
+              {resp_code, <<"00">>}
+              , {resp_msg, <<"success">>}
+              , {orig_resp_code, resp_code}
+              , {orig_resp_msg, resp_msg}
+            },
+            {{pg_mcht_protocol, repo_module, [mcht_txn_log]}, all}
+          ]
+        }
+
+      ]
+    },
+    {fail_resp,
+      [
+        {to, ?MODULE},
+        {from,
+          [
+            {pg_mcht_protocol_req_query, all}
+          ]}
+      ]
+    }
 
   ].
